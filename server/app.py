629c4ced60f1fd95585e49664ec6d2031c297fb2
Jacob Haflett <fivek88@Jacobs-MacBook-Pro.local>
2023-08-10 03:18:42 -0400

    finished adding alert modal

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import uuid
 from flask_cors import CORS 
 
 # instantiate the application
@@ -15,34 +16,61 @@ def ping_pong():
 
 @app.route('/books', methods=['GET', 'POST'])
 def all_books():
-    response_object = {'status':'success'}
+    response_object = {'status': 'success'}
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
-            'title':post_data.get('title'),
-            'author':post_data.get('author'),
-            'read':post_data.get('read')
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
         })
         response_object['message'] = 'Book added!'
     else:
         response_object['books'] = BOOKS
     return jsonify(response_object)
 
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
+
 if __name__ == '__main__':
     app.run()
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True,
     },
-        {
+    {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosoph\'s Stone',
         'author': 'J. K. Rowling',
         'read': True,
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Lord of the Rings: The Hobbit',
         'author': 'J. R. R. Tolkien',
         'read': True,
